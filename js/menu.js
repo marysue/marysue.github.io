926f16c4dda337792b9e3d4d0b7af8378eb5fd43
marysue <mary@marylark.com>
2021-01-06 12:53:43 -0700

    Fixed hamburger menu on iOS

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,36 @@
-$(document).ready(function() {
-	$(".hamburger-icon").on('click', function() {
-	
-		/*alert("HELLO!!!");*/
+// $(document).ready(function() {
+// 	$(".hamburger-icon").on('click', function() {
+
+// 		/*alert("HELLO!!!");*/
+// 		$(".menu").toggleClass("responsive");
+// 	});
+// });
+
+window.addEventListener('DOMContentLoaded', () => {
+	let rootEvent = document.getElementById("rootElement");
+	let burgerNav = document.getElementById("burger-time");
+
+	burgerNav.addEventListener("click", function(e) {
+		console.log("adding open to class list")
+		document.querySelector('nav').classList.add('open');
+		//burgerNav.style.visibility ='hidden';
+
 		$(".menu").toggleClass("responsive");
+
+		e.preventDefault();
+		e.stopPropagation();
+
+
 	});
+
+	closeNav = function(){
+		if (burgerNav.style.visibility === "hidden"){
+		console.log("removing open from class list")
+		   document.querySelector('nav').classList.remove('open');
+		   //burgerNav.style.visibility ='visible';
+		}
+	};
+
+	rootEvent.addEventListener("click", closeNav);
+	rootEvent.addEventListener("touchend", closeNav);
 });
